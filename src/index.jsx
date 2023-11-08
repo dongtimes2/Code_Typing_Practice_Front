4a331541b53ddda9641bdeb474430ce434f7ac9d
dongtimes2 <dongtimes2@gmail.com>
2023-11-09 07:49:28 +0900

    feat: msw 도입 및 환경 세팅

--- src/index.jsx
+++ src/index.jsx
@@ -2,10 +2,20 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App';
 import AppContainer from './container/AppContainer';
+import { worker } from './mock/browser';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <AppContainer>
-    <App />
-  </AppContainer>,
-);
+
+const render = async () => {
+  if (import.meta.env.DEV) {
+    await worker.start();
+  }
+
+  root.render(
+    <AppContainer>
+      <App />
+    </AppContainer>,
+  );
+};
+
+render();
--- src/test/setupTests.js
+++ src/test/setupTests.js
@@ -1,1 +1,10 @@
 import '@testing-library/jest-dom';
+import { afterAll, afterEach, beforeAll } from 'vitest';
+
+import { server } from '../mock/server';
+
+beforeAll(() => server.listen());
+
+afterEach(() => server.resetHandlers());
+
+afterAll(() => server.close());
