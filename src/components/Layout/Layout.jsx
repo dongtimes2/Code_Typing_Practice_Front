a6745be2c717444bf836f02e50e507e0b922b8a5
dongtimes2 <dongtimes2@gmail.com>
2023-11-09 11:10:44 +0900

    feat: 전역 레이아웃 컴포넌트 구현

---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,35 @@
+import { css } from '@emotion/react';
+
+import { HEADER_HEIGHT, SIDEBAR_WIDTH } from '../../constants/layout';
+import Header from '../Header/Header';
+import Sidebar from '../Sidebar/Sidebar';
+
+const contentCss = css`
+  display: flex;
+  padding-top: ${HEADER_HEIGHT};
+  min-height: 100%;
+
+  nav {
+    flex: 0 0 ${SIDEBAR_WIDTH};
+  }
+
+  main {
+    flex: 1;
+  }
+`;
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Header />
+      <div css={contentCss}>
+        <nav>
+          <Sidebar />
+        </nav>
+        <main>{children}</main>
+      </div>
+    </>
+  );
+};
+
+export default Layout;
