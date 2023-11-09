78d56ba169b25e6efc7f53fa57615e86466e8123
dongtimes2 <dongtimes2@gmail.com>
2023-11-09 14:41:35 +0900

    feat: 연습 모드 선택 모달 구현

---
+++ src/pages/Home/components/SelectModal/SelectModal.jsx
@@ -0,0 +1,57 @@
+import { css } from '@emotion/react';
+
+import Button from '../../../../components/Button/Button';
+import theme from '../../../../styles/theme';
+
+const modalCss = css`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 25%;
+  min-width: 24.25rem;
+  height: 40%;
+  min-height: 25rem;
+  padding: 1rem 1rem 2rem;
+  background-color: ${theme.colors.white};
+
+  #title {
+    font-size: ${theme.fontSizes.xxxxl};
+    font-weight: ${theme.fontWeights.bold};
+  }
+
+  .buttonArea {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+    gap: 2rem;
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+const SelectModal = () => {
+  const handleModalClick = (event) => {
+    event.stopPropagation();
+  };
+
+  const handleButtonClick = (type) => {
+    //
+  };
+
+  return (
+    <div css={modalCss} onClick={handleModalClick} data-testid="modal">
+      <p id="title">연습 종류를 선택하세요</p>
+      <div className="buttonArea">
+        <Button onClick={() => handleButtonClick('word')}>낱말 연습</Button>
+        <Button onClick={() => handleButtonClick('short')}>짧은 글 연습</Button>
+        <Button onClick={() => handleButtonClick('long')}>긴 글 연습</Button>
+      </div>
+    </div>
+  );
+};
+
+export default SelectModal;
