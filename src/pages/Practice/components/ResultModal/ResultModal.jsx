c0b37ff918eec83f383f8d0e7dca1b8d7a05e6e5
dongtimes2 <dongtimes2@gmail.com>
2023-11-12 05:36:48 +0900

    feat: 연습 결과 모달 구현

---
+++ src/pages/Practice/components/ResultModal/ResultModal.jsx
@@ -0,0 +1,57 @@
+import { css } from '@emotion/react';
+
+import Button from '../../../../components/Button/Button';
+import { SIDEBAR_WIDTH } from '../../../../constants/layout';
+import { useResultStore } from '../../../../store';
+import theme from '../../../../styles/theme';
+
+const modalCss = css`
+  position: absolute;
+  top: 50%;
+  left: calc(50% + ${SIDEBAR_WIDTH});
+  transform: translate(calc(-50% - (${SIDEBAR_WIDTH} / 2)), -50%);
+  background-color: ${theme.colors.white};
+  display: flex;
+  flex-direction: column;
+  width: 35%;
+  height: 40%;
+  padding: 1rem 1rem 2rem;
+
+  .dataArea {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    font-size: ${theme.fontSizes.xxl};
+    font-weight: ${theme.fontWeights.bold};
+  }
+
+  .buttonArea {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+    gap: 1rem;
+    height: 7.5rem;
+  }
+`;
+
+const ResultModal = ({ onReplayButtonClick, onGoHomeButtonClick }) => {
+  const totalAccuracy = useResultStore((state) => state.totalAccuracy);
+  const totalTypoCount = useResultStore((state) => state.totalTypoCount);
+
+  return (
+    <div css={modalCss} data-testid="modal">
+      <div className="dataArea">
+        <p>정확도: {totalAccuracy}%</p>
+        <p>오타수: {totalTypoCount}개</p>
+      </div>
+      <div className="buttonArea">
+        <Button onClick={onReplayButtonClick}>다시 연습하기</Button>
+        <Button onClick={onGoHomeButtonClick}>첫 화면으로 이동하기</Button>
+      </div>
+    </div>
+  );
+};
+
+export default ResultModal;
