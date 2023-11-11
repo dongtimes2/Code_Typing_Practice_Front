05e40883e024a8e3a73b58af28e45d97043c456a
dongtimes2 <dongtimes2@gmail.com>
2023-11-12 00:12:01 +0900

    chore: react-query 도입 및 환경 설정

--- src/container/AppContainer.jsx
+++ src/container/AppContainer.jsx
@@ -1,13 +1,27 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { HashRouter } from 'react-router-dom';
 
 import GlobalStyles from '../styles/GlobalStyles';
 
 const AppContainer = ({ children }) => {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+        retry: false,
+      },
+    },
+  });
+
   return (
-    <HashRouter>
-      <GlobalStyles />
-      {children}
-    </HashRouter>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={true} />
+      <HashRouter>
+        <GlobalStyles />
+        {children}
+      </HashRouter>
+    </QueryClientProvider>
   );
 };
 
