70cb50caf010d1382a5545c4fda5404e237a7a7a
dongtimes2 <dongtimes2@gmail.com>
2023-11-17 19:20:09 +0900

    feat: electron에서도 네이버 로그인이 가능하게끔 구현

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,7 +1,61 @@
+import { useMutation } from '@tanstack/react-query';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 import NaverButton from './components/NaverButton';
 import Layout from './layout/Layout';
+import { postNaverLogin } from '../../api/login';
+import { PATH } from '../../constants/path';
+import useAuth from '../../hooks/useAuth';
+import { useUserInfoStore } from '../../store';
+import { appRuntime } from '../../utils/appRuntime';
+import { TokenController } from '../../utils/tokenController';
 
 const Login = () => {
+  const navigate = useNavigate();
+
+  const tokenController = new TokenController();
+
+  const setUserNickname = useUserInfoStore((state) => state.setUserNickname);
+  const setUserProfileImage = useUserInfoStore(
+    (state) => state.setUserProfileImage,
+  );
+  const setPracticeNumber = useUserInfoStore(
+    (state) => state.setPracticeNumber,
+  );
+  const setSound = useUserInfoStore((state) => state.setSound);
+  const setIsColorWeakness = useUserInfoStore(
+    (store) => store.setIsColorWeakness,
+  );
+
+  const { setIsLoggedin } = useAuth();
+
+  const { mutate } = useMutation({
+    mutationFn: postNaverLogin,
+    onSuccess: (response) => {
+      tokenController.setAccessToken(response.accessToken);
+      setUserNickname(response.nickname);
+      setUserProfileImage(response.profileImage);
+      setPracticeNumber(response.practiceNumber);
+      setSound(response.sound);
+      setIsColorWeakness(response.isColorWeakness);
+      setIsLoggedin(true);
+      navigate(PATH.HOME);
+    },
+  });
+
+  const [token, setToken] = useState('');
+
+  appRuntime?.on('naverToken', (event, naverToken) => {
+    setToken(naverToken);
+  });
+
+  useEffect(() => {
+    if (token) {
+      mutate(token);
+    }
+  }, [token, mutate]);
+
   return (
     <Layout>
       <div className="moduleArea">
--- src/pages/Login/components/NaverButton.jsx
+++ src/pages/Login/components/NaverButton.jsx
@@ -2,22 +2,41 @@ import { css } from '@emotion/react';
 import { Link } from 'react-router-dom';
 
 import naver from '../../../assets/images/naver.webp';
+import { appRuntime } from '../../../utils/appRuntime';
 
 const buttonCss = css`
   width: 300px;
+  border: none;
 `;
 
 const NaverButton = () => {
-  const link = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${
+  const isElectorn = import.meta.env.VITE_ELECTRON === 'true';
+  const link = `https://nid.naver.com/oauth2.0/authorize?display=popup&response_type=code&client_id=${
     import.meta.env.VITE_NAVER_LOGIN_CLIENT_ID
   }&redirect_uri=${import.meta.env.VITE_NAVER_LOGIN_CALLBACK_URL}&state=${
     import.meta.env.VITE_NAVER_LOGIN_STATE
   }`;
 
+  const handleButtonClick = () => {
+    appRuntime?.send('naverLogin', {
+      clientId: import.meta.env.VITE_NAVER_LOGIN_CLIENT_ID,
+      callbackUrl: import.meta.env.VITE_NAVER_LOGIN_CALLBACK_URL,
+      state: import.meta.env.VITE_NAVER_LOGIN_STATE,
+    });
+  };
+
   return (
-    <Link css={buttonCss} to={link}>
-      <img src={naver} />
-    </Link>
+    <>
+      {isElectorn ? (
+        <button css={buttonCss} onClick={handleButtonClick}>
+          <img src={naver} />
+        </button>
+      ) : (
+        <Link css={buttonCss} to={link}>
+          <img src={naver} />
+        </Link>
+      )}
+    </>
   );
 };
 
