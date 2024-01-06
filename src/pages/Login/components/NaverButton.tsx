58b18be317de38c7a3837a17a3e728048baf6f1f
dongtimes2 <dongtimes2@gmail.com>
2024-01-07 02:05:50 +0900

    feat: 실행 환경에 따라 로그인 요청 url이 달라지도록 구현

--- src/pages/Login/components/NaverButton.tsx
+++ src/pages/Login/components/NaverButton.tsx
@@ -11,18 +11,36 @@ const buttonCss = css`
 
 const NaverButton = () => {
   const isElectorn = import.meta.env.VITE_ELECTRON === 'true';
-  const link = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${
-    import.meta.env.VITE_NAVER_LOGIN_CLIENT_ID
-  }&redirect_uri=${import.meta.env.VITE_NAVER_LOGIN_CALLBACK_URL}&state=${
-    import.meta.env.VITE_NAVER_LOGIN_STATE
-  }`;
+  let link = '';
+
+  if (import.meta.env.DEV) {
+    link = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${
+      import.meta.env.VITE_NAVER_LOGIN_CLIENT_ID_DEV
+    }&redirect_uri=${import.meta.env.VITE_NAVER_LOGIN_CALLBACK_URL_DEV}&state=${
+      import.meta.env.VITE_NAVER_LOGIN_STATE
+    }`;
+  } else if (import.meta.env.PROD) {
+    link = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${
+      import.meta.env.VITE_NAVER_LOGIN_CLIENT_ID
+    }&redirect_uri=${import.meta.env.VITE_NAVER_LOGIN_CALLBACK_URL}&state=${
+      import.meta.env.VITE_NAVER_LOGIN_STATE
+    }`;
+  }
 
   const handleButtonClick = () => {
-    appRuntime?.send('naverLogin', {
-      clientId: import.meta.env.VITE_NAVER_LOGIN_CLIENT_ID,
-      callbackUrl: import.meta.env.VITE_NAVER_LOGIN_CALLBACK_URL,
-      state: import.meta.env.VITE_NAVER_LOGIN_STATE,
-    });
+    if (import.meta.env.DEV) {
+      appRuntime?.send('naverLogin', {
+        clientId: import.meta.env.VITE_NAVER_LOGIN_CLIENT_ID_DEV,
+        callbackUrl: import.meta.env.VITE_NAVER_LOGIN_CALLBACK_URL_DEV,
+        state: import.meta.env.VITE_NAVER_LOGIN_STATE,
+      });
+    } else if (import.meta.env.PROD) {
+      appRuntime?.send('naverLogin', {
+        clientId: import.meta.env.VITE_NAVER_LOGIN_CLIENT_ID,
+        callbackUrl: import.meta.env.VITE_NAVER_LOGIN_CALLBACK_URL,
+        state: import.meta.env.VITE_NAVER_LOGIN_STATE,
+      });
+    }
   };
 
   return (
